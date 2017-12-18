ac4100cbf3985caf7716264ea209f62ddc405ce0
Ariana <ariana.dv4@gmail.com>
2017-12-17 22:30:51 -0600

    created start and finish buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+
+$("#start").on("click", function() {
+
+	$("form").show() ;
+	$("#start").hide();
+
+	//call timer function
+	
+});
+
+
+
+$("#something").on("click", function() {
+
+
+	$("form").hide();
+
+	//display score
+
+
+});
+
+
+
+});
