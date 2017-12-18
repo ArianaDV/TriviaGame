f2f3adc58ab0d03d286253a8730fad161d2b9fff
Ariana <ariana.dv4@gmail.com>
2017-12-18 11:45:59 -0600

    updated score keeping and added if logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,8 @@ var correct = 0;
 var incorrect = 0;
 
 
+
+
 $("#start").on("click", function() {
 
 	$("form").show() ;
@@ -20,24 +22,83 @@ $("#something").on("click", function() {
 
 	q1val = ($("input:radio[name=Question1]:checked", "form" ).val()) ;
 
-	
-	console.log(q1val + " " +"Hey") ;
+	q2val = ($("input:radio[name=Question2]:checked", "form" ).val()) ;
+
+	q3val = ($("input:radio[name=Question3]:checked", "form" ).val()) ;
+
+	q4val = ($("input:radio[name=Question4]:checked", "form" ).val()) ;
+
+	q5val = ($("input:radio[name=Question5]:checked", "form" ).val()) ;
+
+	q6val = ($("input:radio[name=Question6]:checked", "form" ).val()) ;
+
+
+
 
 	if (q1val === "Wendy's") {
 
 		correct ++;
-		console.log("Correct:" + correct) ;
-
-		$("#correct").append(correct);
-
-	} else {
+				
+		} else {
 
 		incorrect ++;
 
-		console.log("Incorrect:" + incorrect) ;
+		}
 
-		$("#incorrect").append(incorrect);
-	}
+
+	if (q2val === "In-N-Out Burger") {
+
+		correct ++;
+				
+		} else {
+
+		incorrect ++;
+
+		}
+
+	if (q3val === "Papa John's") {
+
+		correct ++;
+				
+		} else {
+
+			incorrect ++;
+
+		}
+
+	if (q4val === "KFC") {
+
+		correct ++;
+				
+		} else {
+
+			incorrect ++;
+
+		}
+
+	if (q5val === "Big Mac") {
+
+		correct ++;
+				
+		} else {
+
+			incorrect ++;
+
+		}
+
+	if (q6val === "Sbarro") {
+
+		correct ++;
+				
+		} else {
+
+			incorrect ++;
+
+		}
+	
+	$("#correct").append(correct);
+
+	$("#incorrect").append(incorrect);
 
 
 	$("form").hide();
